1dbcf2436e8ec81dda93ef3a029bc797b27af926
Darkfirm <16521490@mahasiswa.itb.ac.id>
2023-03-13 20:52:31 +0700

    update combination

--- src/Combination.cpp
+++ src/Combination.cpp
@@ -19,21 +19,24 @@ Combination::~Combination(){
 void Combination::setMap(CardInventory hand, CardInventory table){
     for (int i=0; i<hand.getInventory().size(); i++){
         this->addCard(hand.getInventory()[i]);
+        this->hold += hand.getInventory()[i];
     }
     for (int i=0; i<table.getInventory().size(); i++){
         this->addCard(table.getInventory()[i]);
+        this->hold += table.getInventory()[i];
     }
 }
 
 void Combination::addCard(Card input){
     this->mapcard[to_string(input.getAngka())]++;
-    // cout << this->mapcard[to_string(input.getAngka())] << endl;
     this->cardcolor[input.getWarna()]++;
+    this->hold += input;
 }
 
 void Combination::removeCard(Card input){
     this->mapcard[to_string(input.getAngka())]--;
     this->cardcolor[input.getWarna()]--;
+    this->hold -= input;
 }
 
 int Combination::getCardValue(string input){
@@ -53,3 +56,66 @@ void Combination::showCombination(){
     cout << "Hijau : " << this->cardcolor['H'] << endl;
     cout << "Biru : " << this->cardcolor['B'] << endl;
 }
+
+CardInventory getHold() {
+    return this->hold;
+} // mengembalikan inventory di tangan dan table
+
+double getValue() {
+    return this->value->getValue();
+} // mengembalikan value
+
+void setValue(Value *value) {
+    this->value = value;
+} // mengeset pointer value
+
+bool isThere2Angka() {
+    for (int i=0; i<13; i++) {
+        if (this->mapcard[to_string(i)] == 2) {
+            return true;
+        }
+    }
+    return false;
+} // cek apakah ada 2 kartu yang sama
+
+bool isThere3Angka() {
+    for (int i=0; i<13; i++) {
+        if (this->mapcard[to_string(i)] == 3) {
+            return true;
+        }
+    }
+    return false;
+} // cek apakah ada 3 kartu yang sama
+
+bool isThere4Angka() {
+    for (int i=0; i<13; i++) {
+        if (this->mapcard[to_string(i)] == 4) {
+            return true;
+        }
+    }
+    return false;
+} // cek apakah ada 4 kartu yang sama
+
+bool isThere5Warna() {
+    for (int i=0; i<4; i++) {
+        if (this->cardcolor[i] == 5) {
+            return true;
+        }
+    }
+    return false;
+} // cek apakah ada 5 kartu dengan warna yang sama
+
+bool isThereStraight() {
+    int count = 0;
+    for (int i=0; i<13; i++) {
+        if (this->mapcard[to_string(i)] == 1) {
+            count++;
+        } else {
+            count = 0;
+        }
+        if (count == 5) {
+            return true;
+        }
+    }
+    return false;
+} // cek apakah ada 5 kartu dengan angka berurutan
--- src/Combination.hpp
+++ src/Combination.hpp
@@ -11,8 +11,10 @@ using namespace std;
 
 class Combination {
     private:
+        CardInventory hold; // kombinasi kartu
         map<string, int> mapcard; // string = angka ("2", ..., "13"), int = jumlah kartu dengan angka tersebut
         map<char, int> cardcolor;
+        Value *value; // pointer value untuk menghitung nilai kombinasi
     public:
         Combination(); // ctor
         ~Combination(); // dtor
@@ -22,6 +24,14 @@ class Combination {
         int getCardValue(string); // mengembalikan jumlah kartu dengan angka yang sama dengan kartu yang diberikan
         int getCardColor(char); // mengembalikan jumlah kartu dengan warna yang sama dengan kartu yang diberikan
         void showCombination(); // print isi map
+        CardInventory getHold(); // mengembalikan inventory di tangan dan table
+        double getValue(); // mengembalikan value
+        void setValue(Value *value); // mengeset pointer value
+        bool isThere2Angka(); // cek apakah ada 2 kartu yang sama
+        bool isThere3Angka(); // cek apakah ada 3 kartu yang sama
+        bool isThere4Angka(); // cek apakah ada 4 kartu yang sama
+        bool isThere5Warna(); // cek apakah ada 5 kartu dengan warna yang sama
+        bool isThereStraight(); // cek apakah ada 5 kartu dengan angka berurutan
 };
 
 #endif
