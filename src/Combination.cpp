e893b56cba71b3c1a55dfee6c58e044a812cbbec
agshaathalla <agshaathalla@gmail.com>
2023-03-15 01:32:20 +0700

    update combination

--- src/Combination.cpp
+++ src/Combination.cpp
@@ -1,6 +1,9 @@
 #include "Combination.hpp"
 #include <iostream>
 #include <string>
+#include <algorithm>
+#include <array>
+#include <vector>
 
 
 using namespace std;
@@ -33,26 +36,37 @@ Combination::Combination(InventoryHolder hand, InventoryHolder table) {
         this->hold += table.getInventory()[i];
     }
 
-    if (this->isThere4Angka()) {
+    if(isThereStraightFlush()){
+        cout << "YEY STRAIGHT FLUSH" << endl;
+        this->value = new StraightFlush(this->hold, this->mapcard);
+        cout << this->getValue() << endl;
+    }
+    else if (this->isThere4Angka()) {
+        cout << "Four Of A Kind" << endl;
         int i = 1;
         while (this->mapcard[to_string(i)].getInventory().size() < 4) {
             i++;
         }
-        this->value = new FourOfAKind(i); 
+        this->value = new FourOfAKind(i);
+        cout << this->getValue() << endl; 
     }
     else if (this->isThere3Angka() == 2) {
+        cout << "Full House" << endl;
         int i = 13;
         while (this->mapcard[to_string(i)].getInventory().size() < 3) {
             i--;
         }
         this->value = new FullHouse(i);
+        cout << this->getValue() << endl;
     }
     else if (this->isThere3Angka() && this->isThere2Angka()) {
+        cout << "Full House" << endl;
         int i = 1;
         while (this->mapcard[to_string(i)].getInventory().size() < 3) {
             i++;
         }
         this->value = new FullHouse(i);
+        cout << this->getValue() << endl;
     }
     else if (this->isThereFlush()) {
         cout << "YEY FLUSH" << endl;
@@ -72,6 +86,16 @@ Combination::Combination(InventoryHolder hand, InventoryHolder table) {
     else if (this->isThereTwoPair()) {
         cout << "Two Pair" << endl;
         this->value = new TwoPair(this->hold, this->mapcard);
+        cout << this->getValue() << endl;
+    }
+    else if(isTherePair()){
+        cout << "YEY PAIR" << endl;
+        this->value = new Pair(this->mapcard);
+        cout << this->getValue() << endl;
+    }
+    else{
+        cout << "YEY HIGH CARD" << endl;
+        this->value = new HighCard(this->mapcard);
         cout << this->getValue() << endl;
     }
 }
@@ -228,3 +252,116 @@ bool Combination::isThereStraight() {
     return false;
 } // cek apakah ada 5 kartu dengan angka berurutan
 
+bool Combination::isTherePair() {
+    int count = 0;
+    for(int i=13; i>0; i--){
+        if (this->mapcard[to_string(i)].getInventory().size() == 2) {
+            count++;
+        }
+    }
+    if(count == 1){
+        return true;
+    } 
+    else {
+        return false;
+    }
+}
+
+ // cek apakah ada 2 kartu yang sama
+
+bool Combination::isThereStraightFlush() {
+    bool affh = false;
+    int count = 0;
+    string m(1, 'M');
+    string k(1, 'K');
+    string b(1, 'B');
+    string h(1, 'H');
+    if (this->mapcard[m].getInventory().size() >= 5) {
+        int arr[mapcard[m].getInventory().size()];
+        for (int i = 0; i<mapcard[m].getInventory().size(); i++) {
+            arr[i] = mapcard[m].getInventory()[i].getAngka();
+        }
+        int n = sizeof(arr) / sizeof(arr[0]);
+        sort(arr, arr + n);
+
+        for (int i = 1; i<mapcard[m].getInventory().size(); i++) {
+            if (arr[i] == arr[i-1] + 1) {
+                count++;
+                if (count >= 4) {
+                    return true;
+                }
+            }
+            else {
+                count = 0;
+            }
+        }
+        
+    } 
+    else if (this->mapcard[k].getInventory().size() >= 5) {
+        int arr[mapcard[k].getInventory().size()];
+        for (int i = 0; i<mapcard[k].getInventory().size(); i++) {
+            arr[i] = mapcard[k].getInventory()[i].getAngka();
+        }
+        int n = sizeof(arr) / sizeof(arr[0]);
+        sort(arr, arr + n);
+
+        for (int i = 1; i<mapcard[k].getInventory().size(); i++) {
+            if (arr[i] == arr[i-1] + 1) {
+                count++;
+                if (count >= 4) {
+                    return true;
+                }
+            }
+            else {
+                count = 0;
+            }
+        }
+    } 
+    else if (this->mapcard[b].getInventory().size() >= 5) {
+        int arr[mapcard[b].getInventory().size()];
+        for (int i = 0; i<mapcard[b].getInventory().size(); i++) {
+            arr[i] = mapcard[b].getInventory()[i].getAngka();
+        }
+        int n = sizeof(arr) / sizeof(arr[0]);
+        sort(arr, arr + n);
+
+        for (int i = 1; i<mapcard[b].getInventory().size(); i++) {
+            if (arr[i] == arr[i-1] + 1) {
+                count++;
+                if (count >= 4) {
+                    return true;
+                }
+            }
+            else {
+                count = 0;
+            }
+        }
+    } 
+    else if (this->mapcard[h].getInventory().size() >= 5) {
+        int arr[mapcard[h].getInventory().size()];
+        for (int i = 0; i<mapcard[h].getInventory().size(); i++) {
+            arr[i] = mapcard[h].getInventory()[i].getAngka();
+        }
+        int n = sizeof(arr) / sizeof(arr[0]);
+        sort(arr, arr + n);    
+
+        for (int i = 1; i<mapcard[h].getInventory().size(); i++) {
+            if (arr[i] == arr[i-1] + 1) {
+                count++;
+                if (count >= 4) {
+                    return true;
+                }
+            }
+            else {
+                count = 0;
+            }
+        }
+    } 
+    else {
+        return false;
+    }
+    return false;
+
+}
+
+ // cek apakah ada 5 kartu dengan angka berurutan dan warna yang sama (straight dan flush terpenuhi)
