c1fd19f4f3d84d1f1541f10b9701284ee9e5fa3d
christodharma <13521009@std.stei.itb.ac.id>
2023-03-14 15:50:42 +0700

    Merge remote-tracking branch 'origin/main' into christo

--- bin/driver/driver_Combination.cpp
+++ bin/driver/driver_Combination.cpp
@@ -2,63 +2,32 @@
 #include "../../src/Card.cpp"
 #include "../../src/InventoryHolder.cpp"
 #include "../../src/Valueable.cpp"
+#include "../../src/FourOfAKind.cpp"
 #include <iostream>
 using namespace std;
 
 int main(){
     CardInventory invP1;
     Card a('h', 12);
-    Card b('B', 1);
+    Card b('B', 2);
     invP1 += a;
     invP1 += b;
-    CardInventory invP2;
-    Card c(5,'K');
-    Card d(9,'m');
-    invP2 += c;
-    invP2 += d;
-    CardInventory invP3;
-    Card e(5,'K');
-    Card f(9,'m');
-    invP3 += e;
-    invP3 += f;
-    CardInventory invP4;
-    Card g(5,'K');
-    Card h(9,'m');
-    invP4 += g;
-    invP4 += h;
-    CardInventory invP5;
-    Card i(5,'K');
-    Card j(9,'m');
-    invP5 += i;
-    invP5 += j;
-    CardInventory invP6;
-    Card k(5,'K');
-    Card l(9,'m');
-    invP6 += k;
-    invP6 += l;
-    CardInventory invP7;
-    Card m(5,'K');
-    Card n(9,'m');
-    invP7 += m;
-    invP7 += n;
-    CardInventory invTable;
-    Card o(5,'K');
-    Card p(9,'m');
-    Card q(5,'K');
-    Card r(9,'m');
-    Card s(5,'K');
-    invTable += o;
-    invTable += p;
-    invTable += q;
-    invTable += r;
-    invTable += s;
-    Combination map;
-    invP1.showInventory();
-    invTable.showInventory();
-    map.setMap(invP1, invTable);
-    map.showCombination();
-    cout << map.getCardValue("12") << endl;
-    cout << map.getCardColor('H') << endl;
+    CardInventory table;
+    Card c('h', 12);
+    Card d('B', 3);
+    Card e('h', 12);
+    Card f('B', 4);
+    Card g('h', 12);
+    table += c;
+    table += d;
+    table += e;
+    table += f;
+    table += g;
+    Combination com(invP1, table);
+    com.showCombination();
+    cout << com.getByParameter("B").getInventory()[0].getAngka() << endl;
+    cout << com.getByParameter("B").getInventory().size() << endl;
+    cout << com.getValue() << endl;
 
     return 0;
 }
---
+++ src/FourOfAKind.cpp
@@ -0,0 +1,8 @@
+#include "FourOfAKind.hpp"
+
+FourOfAKind::FourOfAKind(CardInventory hold, map<string, CardInventory> mapcard) : Value(0) {
+    this->value = 6;
+}
+
+FourOfAKind::~FourOfAKind() {
+}
