32a68e4d4e5d0a92847c2f1b469c388d2de2facc
Darkfirm <16521490@mahasiswa.itb.ac.id>
2023-03-14 21:05:59 +0700

    add fullhouse

---
+++ src/FullHouse.hpp
@@ -0,0 +1,13 @@
+#ifndef __FULLHOUSE_HPP__
+#define __FULLHOUSE_HPP__
+
+#include "Valueable.hpp"
+#include "InventoryHolder.hpp"
+
+class FullHouse : public Value {
+    public:
+        FullHouse(double i);
+        ~FullHouse();
+};
+
+#endif
